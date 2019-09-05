ca6163b3e45ce9588b53e8fa8db7e3e26b783521
Marlon Agno <mdj.agno@gmail.com>
2019-09-05 12:48:08 -0400

    hello world component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HelloWorld from './state-drills/HelloWorld';
 
+const defaultWhos = ['World', 'Friend', 'React']; 
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld />
+      {console.log(defaultWhos)}
+      <HelloWorld whos={defaultWhos}/>
     </div>
   );
 }
--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -1,22 +1,30 @@
 import React from 'react'
 
 class HelloWorld extends React.Component {
+    static defaultWhos = { whos : [] };
     state = {
-        who: ''
+        whoIndex: 0
     };
 
-    handleClick = () => {
-        this.setState({
-
-        })
+    handleClick(index){
+        this.setState({whoIndex: index})
     }
+   
     render() {
+        const buttons = this.props.whos.map((who, index) => (
+            <button key={index} onClick={this.handleClick}>
+            {this.props.whos(index)}
+            </button>
+          ))
+        const currentWho = this.props.whos[this.state.index]
         return (
             <div>
-                <p>Hello, {this.state.who}</p>
-                <button onClick={this.handleClick}></button>
+                {buttons}
+                <p>Hello, {currentWho}</p>
             </div>
         )
     }
 
 }
+
+export default HelloWorld
